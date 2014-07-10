e5f6dff325dc324e7a311e38a50896aca964e1a5
jb <jbpettit@ebi.ac.uk>
2014-07-10 15:34:44 +0100

    split bug

--- src/main/scala/game/Table.scala
+++ src/main/scala/game/Table.scala
@@ -19,11 +19,119 @@ object Table {
 		Thread.sleep(time)
 		callback()
 	}
+	
+	/*If the round is over, compute winnings/losings*/
+	def roundResolution = {
+		/*Compute gameState to know if all hands are standing, and if we need to make the dealer hit*/
+		var gameState = Player.endOfGameState
+		/*If all hands are standing, resolve game*/
+		if(gameState._2){
+			/*If needed, make the dealer hit his cards*/
+			if(gameState._1){
+				/*Make the dealer draw until 17*/
+				Dealer.unfoldGame
+				this.realTimefeel(()=>{println("Dealer final hand: " + Dealer.hand)},500)
+			}
+			/*Count the points foreach hand*/
+			Player.hands.foreach{hand =>
+				hand.winsAgainstDealer(Dealer.hand) match{
+					/*Lose, you lose your bet*/
+					case Outcomes.Lose => {
+						Player.moneyz -= Player.bet
+						this.realTimefeel(()=>{println("Sorry, you Lose: -"+Player.bet+" chips\n\n")},500)
+					}
+					/*Push, you get back your bet*/
+					case Outcomes.Push => {
+						this.realTimefeel(()=>{println("Push! You keep your bet\n\n")},500)
+					}
+					/*You get twice your bet*/
+					case Outcomes.Win => {
+						Player.moneyz += Player.bet
+						this.realTimefeel(()=>{println("Yeah you win !  +"+Player.bet+" chips\n\n")},500)
+					}
+				}
+			}
+			/*Indicate the player is not playing anymore*/
+			Player.isPlaying=false
+			/*Update the deck*/
+			Deck.updateDeck
+		}
+	}
+	
+	/*One round of play*/
+	def roundOfPlay = {
+		/*Define the user input variable*/
+		var userChoice = 0
+		/*For each hand that isn't standing or busted, let the user decide what to do*/
+		Player.hands.filter{h: Hand => !h.isStanding && !h.isBusted}.foreach{hand =>
+			/*Printing user's available choices*/
+			this.realTimefeel(()=>{
+				println()
+				println(hand)
+				println("You can:")
+				println("1. Hit me !")
+				println("2. Stand")
+				if(hand.isSplittable) {
+					println("3. Split")
+				}
+				}, 500)
+			userChoice = readInt()
+			println(userChoice.toString)
+			userChoice match {
+				case 1 => {
+					Player.hit(hand)
+					/*print last card drawn*/
+					println("You hit: "+hand.cards(hand.cards.length-1))
+					/*if bust, out of the game*/
+					if(hand.isBusted){
+						println("Busted ! score: "+hand.score)
+						hand.isStanding = true
+					}
+				}
+				case 2 => hand.isStanding = true
+				case 3 => Player.split(hand)
+			}
+		}
+		
+		/*Resolve the round if needed*/
+		this.roundResolution
+		
+		this.play
+
+	}
+	
+	/*Place the bets and start the round*/
+	def startRound {
+		/*Define the user input variable*/
+		var userChoice = 0
+		/*intialize the player state at the start of the game*/
+		Player.initNewGame
+		/*Empty the dealer's hand*/
+		Dealer.hand.emptyHand
+		println("Wanna play?. Enter the table with a bet of how many? (min 1, 0 to exit program). you have "+Player.moneyz+" chips available") 
+		userChoice = readInt()
+		/*Exit if needed*/
+		if(userChoice > 0){
+			this.realTimefeel(()=>{println("Let's Go!")}, 500)
+			/*Place the bet per hand*/
+			Player.placeBet(userChoice)
+			/*Hit the dealer's first card*/
+			Dealer.hit
+			/*Inform user of dealer's first card*/
+			this.realTimefeel(()=>{println("Dealer just hit:" +Dealer.hand+"\n")}, 500)
+
+
+			/*Hit the user's first two cards which are in the first hand*/
+			Player.hit(Player.hands(0));
+			this.realTimefeel(()=>{println("You just hit:" +Player.hands(0).cards(0))}, 500)
+			Player.hit(Player.hands(0));
+			this.realTimefeel(()=>{println("you just hit:" +Player.hands(0).cards(1))}, 500)
+			this.play
+		}
+	}
 
 	/*Main game recursive function, play until quit of out of money*/
 	def play:Unit = {
-		/*Define the user input variable*/
-		var userChoice = 0
 		/*Try to play otherwise catch the exception*/
 		try{
 			/*Does the player still have money? If not exit*/
@@ -34,99 +142,11 @@ object Table {
 					/*Is the player in the middle of a game?*/
 					Player.isPlaying match {
 						case true => {
-							/*For each hand that isn't standing or busted, let the user decide what to do*/
-							Player.hands.filter{h: Hand => !h.isStanding && !h.isBusted}.foreach{hand =>
-								/*Printing user's available choices*/
-								this.realTimefeel(()=>{
-									println()
-									println(hand)
-									println("You can:")
-									println("1. Hit me !")
-									println("2. Stand")
-									if(hand.isSplittable) {
-										println("3. Split")
-									}
-									}, 500)
-								userChoice = readInt()
-								println(userChoice.toString)
-								userChoice match {
-									case 1 => {
-										Player.hit(hand)
-										/*print last card drawn*/
-										println("You hit: "+hand.cards(hand.cards.length-1))
-										/*if bust, out of the game*/
-										if(hand.isBusted){
-											println("Busted ! score: "+hand.score)
-											hand.isStanding = true
-										}
-									}
-									case 2 => hand.isStanding = true
-									case 3 => Player.split(hand)
-								}
-							}
-
-							/*Compute gameState to know if all hands are standing, and if we need to make the dealer hit*/
-							var gameState = Player.endOfGameState
-							/*If all hands are standing, resolve game*/
-							if(gameState._2){
-								/*If needed, make the dealer hit his cards*/
-								if(gameState._1){
-									/*Make the dealer draw until 17*/
-									Dealer.unfoldGame
-									this.realTimefeel(()=>{println("Dealer final hand: " + Dealer.hand)},500)
-								}
-								/*Count the points foreach hand*/
-								Player.hands.foreach{hand =>
-									hand.winsAgainstDealer(Dealer.hand) match{
-										/*Lose, you lose your bet*/
-										case Outcomes.Lose => {
-											Player.moneyz -= Player.bet
-											this.realTimefeel(()=>{println("Sorry, you Lose: -"+Player.bet+" chips\n\n")},500)
-										}
-										/*Push, you get back your bet*/
-										case Outcomes.Push => {
-											this.realTimefeel(()=>{println("Push! You keep your bet\n\n")},500)
-										}
-										/*You get twice your bet*/
-										case Outcomes.Win => {
-											Player.moneyz += Player.bet
-											this.realTimefeel(()=>{println("Yeah you win !  +"+Player.bet+" chips\n\n")},500)
-										}
-									}
-								}
-								/*Indicate the player is not playing anymore*/
-								Player.isPlaying=false
-								/*Update the deck*/
-								Deck.updateDeck
-							}
-							this.play
-
+							this.roundOfPlay
 						}
 						/*Player isn't playing, start a new game by betting some chips*/
 						case false => {
-							/*intialize the player state at the start of the game*/
-							Player.initNewGame
-							Dealer.hand.emptyHand
-							println("Wanna play?. Enter the table with a bet of how many? (min 1, 0 to exit program). you have "+Player.moneyz+" chips available") 
-							userChoice = readInt()
-							/*Exit if needed*/
-							if(userChoice > 0){
-								this.realTimefeel(()=>{println("Let's Go!")}, 500)
-								/*Place the bet per hand*/
-								Player.placeBet(userChoice)
-								/*Hit the dealer's first card*/
-								Dealer.hit
-								/*Inform user of dealer's first card*/
-								this.realTimefeel(()=>{println("Dealer just hit:" +Dealer.hand+"\n")}, 500)
-
-
-								/*Hit the user's first two cards which are in the first hand*/
-								Player.hit(Player.hands(0));
-								this.realTimefeel(()=>{println("You just hit:" +Player.hands(0).cards(0))}, 500)
-								Player.hit(Player.hands(0));
-								this.realTimefeel(()=>{println("you just hit:" +Player.hands(0).cards(1))}, 500)
-								this.play
-							}
+							this.startRounf
 						}
 					}
 				}
